8b6ce74ecd60f1fa0b15811bcc3279523f90a155
ShaquilleTL <labistours@gmail.com>
2022-09-08 12:05:51 -0700

    MultilinguaL Puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-
+/**
+ * main -entry point for the program
+ *
+ * return: display a message to the terminal.
+ */
 int main(void) 
 {
 
-	puts("\" Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
