96bd216cd036377ac71fee999da19625e3203060
ShaquilleTL <labistours@gmail.com>
2022-09-09 11:00:51 -0700

    Print alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Print statements based on the last digit of the random number
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+{
+	putchar(alpha);
+	alpha++;
+}
+putchar('\n');
+return (0);
+}
