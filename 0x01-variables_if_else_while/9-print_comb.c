65c32a58758d58b6afb373a3563e6d27d31d8891
ShaquilleTL <labistours@gmail.com>
2022-09-09 19:59:36 -0700

    Added solution

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program will be executed here
+ *
+ * Description: Prints all possible combinations of
+ * single digit numbers
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
