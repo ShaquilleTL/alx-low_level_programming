9f837d00651851017709a48b89c3af66894c4b72
ShaquilleTL <labistours@gmail.com>
2022-09-14 04:49:10 -0700

    add two numbers and print the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @x: type int number
+ * @y: type int number
+ * Result: sum of the two integers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
