15ca4481f678f49332e647199d176e12c5c79eca
ShaquilleTL <labistours@gmail.com>
2022-09-15 03:41:31 -0700

    Added solution

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal -draw a diagonal line
+ * @n: number of times the backslash is printed
+ * Description: prints a diagonal line using backslash
+ */
+void print_diagonal(int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+	if (c < 1)
+		_putchar('\n');
+}
