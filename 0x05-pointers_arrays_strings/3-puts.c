e7c58bb57116caf0d911312f80e728c9d6585828
ShaquilleTL <labistours@gmail.com>
2022-09-19 10:30:58 -0700

    added _putchar to header

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,16 +3,16 @@
 /**
  * _puts - print a string to stdout
  * @str: type char array string
- * Description: Fear the lack of pcs
+ * _putchar - used to print
  */
 
 void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] !='\0'; i++)
 	{
 		_putchar(str[i]);
 }
-_putchar('\n');
+	_putchar('\n');
 }
