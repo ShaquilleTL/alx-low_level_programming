c3f0b62fc8dbf8be0d2e0300b7d169bfcec34fd9
ShaquilleTL <labistours@gmail.com>
2022-09-23 11:55:25 -0700

    Fixed the code

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,15 +9,15 @@
 char *rot13(char *s)
 {
 	int i;
-	char alphaA[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char alphaB[] = "nopqrstuvwxyzabcdefghijklm";
+	char alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char rotate[] = "nopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
 		{
 			s[i] = (s[i] - 65 > 25) ?
-				alphaA[s[i] - 97] : alphaB[s[i] - 65];
+				rotate[s[i] - 97] : alphabet[s[i] - 65];
 		}
 	}
 	return (s);
