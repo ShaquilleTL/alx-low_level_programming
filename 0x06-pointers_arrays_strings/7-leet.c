4c7e0c1238cc83f88453d343c0e17cc2a643ec33
ShaquilleTL <labistours@gmail.com>
2022-09-23 13:41:09 -0700

    Changed code to meet conditions

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,26 @@
 
 char *leet(char *s)
 {
+	char *c; /* checks for */
+	char *r; /* will replace */
 	int i;
+	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	c = "aeotl";
+	r = "43071";
+	i = 0;
+	while (c[i] != '\0')
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[j] == c[i] || s[j] == (c[i] - 32))
+			{
+				s[j] = r[i];
+			}
+			j++;
+		}
+		i++;
 	}
 	return (s);
 }
