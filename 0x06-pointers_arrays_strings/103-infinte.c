370af5f1756968e3d24c904490e1586e4ebac7ab
ShaquilleTL <labistours@gmail.com>
2022-09-26 11:08:09 -0700

    start afresh

---
+++ 0x06-pointers_arrays_strings/103-infinte.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * infinte_add - adds two numbers
+ * @n1: 1 number to be added
+ * @n2: 2 number to be added
+ * @r: buffer
+ * @size_r: buffer size
+ * Return: sum of two integers 
+ */
+
+char *infinte_add(char *n1, char *n2, char *r, int size_r)
+{
+
+	int sum;
