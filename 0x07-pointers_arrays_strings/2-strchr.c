1f606932475663fa5aeb6577e1644995a6554290
ShaquilleTL <labistours@gmail.com>
2022-09-27 05:11:08 -0700

    Remove whitespace

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - locate character in a string
  * @s: char array string
  * @c: char to look for
- * Return: NULL if char not found, or pointer 
+ * Return: NULL if char not found, or pointer
  */
 
 char *_strchr(char *s, char c)
