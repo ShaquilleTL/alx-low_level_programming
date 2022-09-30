97d4ee388b47a8b7f9d563008bdb9689089957ec
ShaquilleTL <labistours@gmail.com>
2022-09-30 04:57:52 -0700

    Added solution

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments that it receives
+ * @argc: counts the arguments
+ * @argv: vector argument and array of string
+ * Return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
