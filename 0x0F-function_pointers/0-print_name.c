56be67c41c368f46ad7be3229b1dc8920414f746
ShaquilleTL <labistours@gmail.com>
2022-10-13 02:11:12 -0700

    Added if NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - print a name from a function pointer
@@ -8,5 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	f(name);
 }
