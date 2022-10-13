92ce342c1a22312bb9144ffc3c71361565d2a479
ShaquilleTL <labistours@gmail.com>
2022-10-13 02:18:40 -0700

    added if statement

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * array_iterator - executes a function pointer on each element
@@ -10,6 +11,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
+	if (action == NULL || array ++ NULL)
+		return;
 
 	for(i = 0; i < size; i++)
 		action(array[i]);
